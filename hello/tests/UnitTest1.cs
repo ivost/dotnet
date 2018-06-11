71acf9a9f20d003f1391daee47a74a2c324a95a5
Ivostoysd <ivo.stoyanov@smartdrive.net>
2018-06-10 19:15:08 -0700

    sln

---
+++ hello/tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using logic;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        	Assert.Equal("Hello, Ivo", Class1.Greet("Ivo"));
+        }
+    }
+}
