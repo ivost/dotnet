3440b1ac0ffcb68f4f42a1c1192f3b9342d1533a
ivost <ivostoy@yahoo.com>
2018-06-21 19:36:14 -0700

    test

--- msync/winser/Program.cs
+++ msync/winser/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+using System;
+using System.IO.Ports;
 
+/*
+ * 
+https://docs.microsoft.com/en-us/dotnet/api/system.io.ports.serialport?view=netcore-2.0
+*/
 namespace winser
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+	        var port = new System.IO.Ports.SerialPort("COM3", 19200);
+
+	        port.Open();
+
+            Console.WriteLine("port open");
         }
     }
 }
